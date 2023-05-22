f5cea095567dcc8e91f928ef0dfcd51f81908422
carullahtursun <62027425+carullahtursun@users.noreply.github.com>
2023-05-22 13:48:01 +0300

    port

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dns from 'dns'
 
+dns.setDefaultResultOrder('verbatim')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080,
+  },
+  preview: {
+    port: 8080,
+  },
 
 })
 
