b977f0ed119d1f4032e94673b778eee30e32a571
tarik.fi <tarik.ucar@avsos.ai>
2023-06-02 18:50:41 +0300

    notification URL

--- server/routers/paytr.js
+++ server/routers/paytr.js
@@ -112,6 +112,38 @@ router.post("/create-payment", function (req, res) {
 
 });
 
+router.post("/callback", function (req, res) {
+
+    // ÖNEMLİ UYARILAR!
+    // 1) Bu sayfaya oturum (SESSION) ile veri taşıyamazsınız. Çünkü bu sayfa müşterilerin yönlendirildiği bir sayfa değildir.
+    // 2) Entegrasyonun 1. ADIM'ında gönderdiğniz merchant_oid değeri bu sayfaya POST ile gelir. Bu değeri kullanarak
+    // veri tabanınızdan ilgili siparişi tespit edip onaylamalı veya iptal etmelisiniz.
+    // 3) Aynı sipariş için birden fazla bildirim ulaşabilir (Ağ bağlantı sorunları vb. nedeniyle). Bu nedenle öncelikle
+    // siparişin durumunu veri tabanınızdan kontrol edin, eğer onaylandıysa tekrar işlem yapmayın. Örneği aşağıda bulunmaktadır.
+
+    var callback = req.body;
+
+    // POST değerleri ile hash oluştur.
+    paytr_token = callback.merchant_oid + merchant_salt + callback.status + callback.total_amount;
+    var token = crypto.createHmac('sha256', merchant_key).update(paytr_token).digest('base64');
+
+    // Oluşturulan hash'i, paytr'dan gelen post içindeki hash ile karşılaştır (isteğin paytr'dan geldiğine ve değişmediğine emin olmak için)
+    // Bu işlemi yapmazsanız maddi zarara uğramanız olasıdır.
+
+    if (token != callback.hash) {
+        throw new Error("PAYTR notification failed: bad hash");
+    }
+
+    if (callback.status == 'success') {
+        //basarili
+    } else {
+        //basarisiz
+    }
+
+    res.send('OK');
+
+});
+
 
 
 module.exports = router;
